85ae098f2682deae859180401354116d56486b94
Artiatech Studio <artiateech@gmail.com>
2026-03-05 14:22:45 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6598686877-c9281",
+  "appId": "1:726919145424:web:a2832af624b391518f7bd5",
+  "apiKey": "AIzaSyAVlpkH2kD5UAXUf1wBBKTel2q7enViTuw",
+  "authDomain": "studio-6598686877-c9281.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "726919145424"
+};
